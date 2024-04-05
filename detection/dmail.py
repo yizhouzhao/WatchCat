1e98873de9ca9fd8d9c5321ea3bfabe61bb3d2f6
DigitalBotLab <yizhou_zhao@berkeley.edu>
2024-04-05 16:37:16 -0700

    new email pipeline

--- detection/dmail.py
+++ detection/dmail.py
@@ -1,20 +1,22 @@
 import sendgrid
 import os
+from sendgrid.helpers.mail import *
 
 from dotenv import load_dotenv
 load_dotenv() 
 
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 
-print("SENDGRID_API_KEY: ", SENDGRID_API_KEY)
+# print("SENDGRID_API_KEY: ", SENDGRID_API_KEY)
 
-# sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
-# from_email = Email("test@example.com")
-# to_email = To("test@example.com")
-# subject = "Sending with SendGrid is Fun"
-# content = Content("text/plain", "and easy to do anywhere, even with Python")
-# mail = Mail(from_email, to_email, subject, content)
-# response = sg.client.mail.send.post(request_body=mail.get())
-# print(response.status_code)
-# print(response.body)
-# print(response.headers)
+def send_email():
+    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("home@digitalbotlab.com")
+    to_email = To("yizhou@digitalbotlab.com")
+    subject = "Sending with SendGrid is Fun"
+    content = Content("text/html", "and easy to do anywhere, even with Python")
+    mail = Mail(from_email, to_email, subject, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
